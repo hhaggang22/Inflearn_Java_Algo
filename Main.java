3c6d78a6291a70ff997e95c0d189c0da306fee50
hhaggang22 <20181018@sungshin.ac.kr>
2022-03-04 17:37:00 +0900

    1. 문자찾기

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+  
+public class Main {
+    
+    public int solution(String str, char c){
+        int answer = 0;
+
+        str = str.toUpperCase();
+        c = Character.toUpperCase(c);
+        
+        for(int i = 0 ; i < str.length(); i++){
+            if(str.charAt(i)==c) answer++;
+        }
+
+        return answer;
+    }
+  public static void main(String[] args){
+
+    Main T = new Main();
+
+    Scanner in = new Scanner(System.in);
+    String str = in.next();
+    char c = in.next().charAt(0);
+
+    System.out.print(T.solution(str, c));
+
+  }
+}
