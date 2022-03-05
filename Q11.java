53f720297c76a099d437ed484f52642168a495d7
hhaggang22 <20181018@sungshin.ac.kr>
2022-03-05 13:33:07 +0900

    11.문자열압축

---
+++ Q11.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Q11 {
+    public String solution(String str){
+        String answer = "";
+        str = str+" ";
+        int cnt = 1;
+
+        for(int i=0; i < str.length()-1; i++){
+            if(str.charAt(i)==str.charAt(i+1)){
+                cnt++;
+            }
+            else{
+                answer += str.charAt(i);
+                if(cnt > 1){
+                    answer += String.valueOf(cnt);
+                }
+                cnt = 1;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Q11 T = new Q11();
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        System.out.print(T.solution(str));
+    }
+}
