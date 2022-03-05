22433ba57977c14ce752f40b7d55cbeb1b43e832
hhaggang22 <20181018@sungshin.ac.kr>
2022-03-05 13:08:02 +0900

    10.가장 짧은 문자거리

---
+++ Q10.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Q10 {
+    public int[] solution(String str, char c){
+        int[] answer = new int[str.length()];
+        int p = 1000;
+
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == c){
+                p = 0;
+                answer[i] = p;
+            }
+            else{
+                p++;
+                answer[i] = p;
+            }
+        }
+
+        p = 1000; //다시 초기화 해줘야함
+        for(int k = str.length()-1; k > -1; k--){
+            if(str.charAt(k) == c){
+                p = 0; 
+                answer[k] = p; //이미 들어가있으니까 굳이 해줄 필요 없음
+            }
+            else{
+                p++;
+                if(answer[k] > p) answer[k] = p;
+                //answer[k] = Math.min(answer[k], p);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Q10 T = new Q10();
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        char c = sc.next().charAt(0);
+        
+        for(int x : T.solution(str, c)){
+            System.out.print(x + " ");
+        }
+    }
+    
+}
---
+++ Q9.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Q9 {
+    public int solution(String str) {
+        String tmp = "";
+        int answer = 0;
+
+        /*
+         * tmp = str.toUpperCase().replaceAll("[A-Z]","");
+         */
+
+        for (char x : str.toCharArray()) {
+            if (Character.isDigit(x)) {
+                tmp += x;
+            }
+        }
+
+        answer = Integer.parseInt(tmp);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Q9 T = new Q9();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.print(T.solution(str));
+    }
+}
