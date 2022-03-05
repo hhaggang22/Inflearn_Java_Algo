40f4a521f25739a80b456b51b2c846f9319e6942
hhaggang22 <20181018@sungshin.ac.kr>
2022-03-05 11:44:31 +0900

    6.중복문자제거

---
+++ Q6.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Q6 {
+    public String solution(String str){
+        String answer = "";
+        for(int i = 0; i < str.length() ; i++){
+            if(str.indexOf(str.charAt(i))==i){
+                answer+=str.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Q6 T = new Q6();
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        System.out.print(T.solution(str));
+    }
+    
+}
