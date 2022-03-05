1c09c685860f8ad9098607dd58c8a19599317ec2
hhaggang22 <20181018@sungshin.ac.kr>
2022-03-05 11:07:22 +0900

    4.단어뒤집기

---
+++ Q4.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Q4 {
+
+    public ArrayList<String> solution(int n, String[] str){
+        ArrayList<String> answer = new ArrayList<>();
+
+        /*for(String x : str){
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }*/
+
+        for(String x : str){
+            char[] s = x.toCharArray();
+            int lt = 0, rt = x.length()-1;
+
+            while(lt < rt){
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(s); //x로 하면 안됨
+            answer.add(tmp);
+
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Q4 T = new Q4();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        String[] str = new String[n];
+        for(int i=0; i<n; i++){
+            str[i] = sc.next();
+        }
+
+        for(String t : T.solution(n, str)){
+            System.out.println(t);
+        }
+    }
+    
+}
