61954bc3c879581ea311e5beec276988c95464b8
hhaggang22 <20181018@sungshin.ac.kr>
2022-03-11 00:23:03 +0900

    7.회문 문자열(내가 생각한 답 추가)

--- Q7.java
+++ Q7.java
@@ -4,13 +4,27 @@ public class Q7 {
 
     public String solution(String str){
         String answer = "YES";
+        String temp  = "";
         str = str.toUpperCase();
 
+        /*
         for(int i = 0; i < str.length()/2; i++){
             if(str.charAt(i)!=str.charAt(str.length()-1-i)){
                 answer = "NO";
             }
         }
+        */
+        
+
+        temp = new StringBuilder(str).reverse().toString();
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i)!= temp.charAt(i)){
+                answer = "NO";
+                break;
+            }
+        }
+
+        
 
         return answer;
     }
