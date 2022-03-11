c892dff36beb02d6eac3fbd352009ffee0fbdf55
hhaggang22 <20181018@sungshin.ac.kr>
2022-03-11 17:49:32 +0900

    11.문자열 압축

--- String/Q11.java
+++ String/Q11.java
@@ -25,9 +25,7 @@ public class Q11 {
     public static void main(String[] args){
         Q11 T = new Q11();
         Scanner sc = new Scanner(System.in);
-
         String str = sc.next();
-
         System.out.print(T.solution(str));
     }
 }
