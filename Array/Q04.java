71e56b4c98dc959ed0a40066a9ecc871a3dd80aa
hhaggang22 <20181018@sungshin.ac.kr>
2022-03-11 23:43:47 +0900

    4.피보나치 수열

---
+++ Array/Q04.java
@@ -0,0 +1,30 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Q04 {
+    public int[] solution(int n){
+        int[] answer = new int[n];
+        answer[0] = 1;
+        answer[1] = 1;
+
+        for(int i=2; i<n; i++){
+            answer[i] = answer[i-2] + answer[i-1];
+        }
+        
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Q04 T = new Q04();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int x : T.solution(n)){
+            System.out.print(x+" ");
+        }
+    }
+    
+}
